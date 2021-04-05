d8cae2367aaae03ebb89fc46ebdd4f4b92984af4
Vyankat, Sattarwar <Vyankat.Sattarwar@cdk.com>
2021-04-05 15:18:14 +0530

    added combnation test cases

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,15 +3,20 @@ package com.gildedrose;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import org.junit.jupiter.api.Test;
+import static org.approvaltests.combinations.CombinationApprovals.verifyAllCombinations;
 class GildedRoseTest {
-
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        verifyAllCombinations(
+                this :: doUpdateQuality,
+                new String[]  {"foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"},
+                new Integer[] { -1, 0, 2, 6, 11 },
+                new Integer[] { 0, 1, 49, 50 } );
+    }
+    private String doUpdateQuality (String name, int sellIn, int quality) {
+        Item[] items = new Item[] { new Item(name, sellIn, quality)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
-    }
-
-}
+        return app.items[0].toString();
+    }}
