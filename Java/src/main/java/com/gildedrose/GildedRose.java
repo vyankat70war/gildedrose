f448492ef84b13ca579309a6401346aa621702d1
“Ashwin <Ashwin.Joshi@cdk.com>
2021-04-26 15:21:15 +0530

    r- Removed unused code

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -39,17 +39,15 @@ class GildedRose {
                 if (item.quality < 50) {
                     item.quality++;
 
-                    if (item.name.equals(BACKSTAGE_PASSES)) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality++;
-                            }
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            item.quality++;
                         }
+                    }
 
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality++;
-                            }
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            item.quality++;
                         }
                     }
                 }
@@ -60,20 +58,14 @@ class GildedRose {
             }
 
             if (item.sellIn < 0) {
-                if (!item.name.equals(AGED_BRIE)) {
-                    if (!item.name.equals(BACKSTAGE_PASSES)) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals(SULFURAS)) {
-                                item.quality--;
-                            }
+                if (!item.name.equals(BACKSTAGE_PASSES)) {
+                    if (item.quality > 0) {
+                        if (!item.name.equals(SULFURAS)) {
+                            item.quality--;
                         }
-                    } else {
-                        item.quality = item.quality - item.quality;
                     }
                 } else {
-                    if (item.quality < 50) {
-                        item.quality++;
-                    }
+                    item.quality = item.quality - item.quality;
                 }
             }
         }
