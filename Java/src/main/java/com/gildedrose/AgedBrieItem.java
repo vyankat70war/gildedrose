bb886d8c9d8946ef653205cc548f9dc9ac82f620
“Ashwin <Ashwin.Joshi@cdk.com>
2021-05-24 15:45:25 +0530

    r- Moved implemenation to subclasses.

--- Java/src/main/java/com/gildedrose/AgedBrieItem.java
+++ Java/src/main/java/com/gildedrose/AgedBrieItem.java
@@ -7,59 +7,17 @@ public class AgedBrieItem extends Item {
 
     @Override
     void updateQuality() {
-        switch (name) {
-            case AGED_BRIE:
-                if (quality < 50) {
-                    quality++;
+        if (quality < 50) {
+            quality++;
 
-                }
+        }
 
-                sellIn--;
+        sellIn--;
 
-                if (sellIn < 0) {
-                    if (quality < 50) {
-                        quality++;
-                    }
-                }
-                break;
-            case BACKSTAGE_PASSES:
-                if (quality < 50) {
-                    quality++;
-
-                    if (sellIn < 11) {
-                        if (quality < 50) {
-                            quality++;
-                        }
-                    }
-
-                    if (sellIn < 6) {
-                        if (quality < 50) {
-                            quality++;
-                        }
-                    }
-                }
-
-                sellIn--;
-                if (sellIn < 0) {
-                    quality = 0;
-                }
-                break;
-            case SULFURAS:
-
-                break;
-            default:
-                if (quality > 0) {
-                    quality--;
-                    sellIn--;
-                    if (sellIn < 0) {
-                        if (quality > 0) {
-                            quality--;
-                        }
-                    }
-                } else {
-                    sellIn--;
-                }
-                break;
+        if (sellIn < 0) {
+            if (quality < 50) {
+                quality++;
+            }
         }
     }
 }
--- Java/src/main/java/com/gildedrose/BackstagePassesItem.java
+++ Java/src/main/java/com/gildedrose/BackstagePassesItem.java
@@ -7,59 +7,26 @@ public class BackstagePassesItem extends Item {
 
     @Override
     void updateQuality() {
-        switch (name) {
-            case AGED_BRIE:
+        if (quality < 50) {
+            quality++;
+
+            if (sellIn < 11) {
                 if (quality < 50) {
                     quality++;
-
                 }
+            }
 
-                sellIn--;
-
-                if (sellIn < 0) {
-                    if (quality < 50) {
-                        quality++;
-                    }
-                }
-                break;
-            case BACKSTAGE_PASSES:
+            if (sellIn < 6) {
                 if (quality < 50) {
                     quality++;
-
-                    if (sellIn < 11) {
-                        if (quality < 50) {
-                            quality++;
-                        }
-                    }
-
-                    if (sellIn < 6) {
-                        if (quality < 50) {
-                            quality++;
-                        }
-                    }
                 }
-
-                sellIn--;
-                if (sellIn < 0) {
-                    quality = 0;
-                }
-                break;
-            case SULFURAS:
-
-                break;
-            default:
-                if (quality > 0) {
-                    quality--;
-                    sellIn--;
-                    if (sellIn < 0) {
-                        if (quality > 0) {
-                            quality--;
-                        }
-                    }
-                } else {
-                    sellIn--;
-                }
-                break;
+            }
         }
+
+        sellIn--;
+        if (sellIn < 0) {
+            quality = 0;
+        }
+
     }
 }
