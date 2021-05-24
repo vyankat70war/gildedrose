185cdfba918502d47161b8451924f6cd4bcae965
“Ashwin <Ashwin.Joshi@cdk.com>
2021-05-24 15:39:01 +0530

    r- inlined method to subclasses.

--- Java/src/main/java/com/gildedrose/BackstagePassesItem.java
+++ Java/src/main/java/com/gildedrose/BackstagePassesItem.java
@@ -7,6 +7,59 @@ public class BackstagePassesItem extends Item {
 
     @Override
     void updateQuality() {
-        super.updateQuality();
+        switch (name) {
+            case AGED_BRIE:
+                if (quality < 50) {
+                    quality++;
+
+                }
+
+                sellIn--;
+
+                if (sellIn < 0) {
+                    if (quality < 50) {
+                        quality++;
+                    }
+                }
+                break;
+            case BACKSTAGE_PASSES:
+                if (quality < 50) {
+                    quality++;
+
+                    if (sellIn < 11) {
+                        if (quality < 50) {
+                            quality++;
+                        }
+                    }
+
+                    if (sellIn < 6) {
+                        if (quality < 50) {
+                            quality++;
+                        }
+                    }
+                }
+
+                sellIn--;
+                if (sellIn < 0) {
+                    quality = 0;
+                }
+                break;
+            case SULFURAS:
+
+                break;
+            default:
+                if (quality > 0) {
+                    quality--;
+                    sellIn--;
+                    if (sellIn < 0) {
+                        if (quality > 0) {
+                            quality--;
+                        }
+                    }
+                } else {
+                    sellIn--;
+                }
+                break;
+        }
     }
 }
