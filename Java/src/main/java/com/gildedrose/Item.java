532a62463c888392915ef0a670b87ce31f9d9f12
Vyankat, Sattarwar <Vyankat.Sattarwar@cdk.com>
2021-05-24 15:08:04 +0530

    r- made name final

--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -5,16 +5,17 @@ public class Item {
     public static final String AGED_BRIE = "Aged Brie";
     public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
     public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
-    public String name;
+    public final String name;
 
     public int sellIn;
 
     public int quality;
 
-    public Item(String name, int sellIn, int quality) {
+    public Item(final String name, int sellIn, int quality) {
         this.name = name;
         this.sellIn = sellIn;
         this.quality = quality;
+
     }
 
    @Override
